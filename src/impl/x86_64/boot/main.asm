30a2d46b4f8c273917e02284502a6076487ed484
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-16 23:59:09 +0530

    implementing paging

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -9,6 +10,11 @@ start:
     call check_cpuid
     call check_long_mode
 
+    call setup_page_tables
+    call enable_paging
+
+    lgdt [gdt64.pointer]
+    jmp gdt64.code_segment:long_mode_start
     
 
     ; print 'OK'
@@ -57,6 +63,51 @@ check_long_mode:
     mov al, "L" ;error code L for no long_mode
     jmp error
 
+setup_page_tables:
+    mov eax, page_table_l3
+    or eax, 0b11 ; present, writable
+    MOV [page_table_l4], eax
+
+    mov eax, page_table_l2
+    or eax, 0b11 ; present, writable
+    MOV [page_table_l3], eax
+
+    mov ecx, 0 ; counter
+.loop:
+    mov eax, 0x200000 ; 2 MiB
+    mul ecx
+    or eax, 0b10000011 ; presemt, writable, huge page
+    mov [page_table_l2 + ecx * 8], eax
+
+    inc ecx ; increment counter
+    cmp ecx, 512 ; checks whether the whole table is mapped
+    jne .loop ; if not, continue
+
+    ret
+
+enable_paging:
+    ; pass the page table location to cpu
+    mov eax, page_table_l4
+    mov cr3, eax
+
+    ; enable Physical Address Extension
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; enable long mode
+    mov ecx, 0xC0000080
+    rdmsr 
+    or eax, 1 << 8
+    wrmsr
+
+    ;enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
 error:
     ; print "ERR:X" where X denotes the error code
     mov dword [0xb8000], 0x4f524f45
@@ -67,6 +118,22 @@ error:
 
 
 section .bss
+align 4096
+page_table_l4:
+    resb 4096
+page_table_l3:
+    resb 4096
+page_table_l2:
+    resb 4096
 stack_bottom:
     resb 4069 * 4
 stack_top:
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+.code_segment: equ $ - gdt64
+    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53); code segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
