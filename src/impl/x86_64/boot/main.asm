335f5eb7c825518627d8a867a760992e58b12c41
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-16 22:44:21 +0530

    adding cpuid recognition

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -5,9 +5,35 @@ bits 32
 start:
     mov esp, stack_top
 
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
     ; print 'OK'
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+.no_multiboot:
+    mov al, "M"
+    jmp error
+
+check_cpuid:
+    pushfd
+    pop eax
+    mov ecx, eax
+
+error:
+    ; print "ERR:X" where X denotes the error code
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
+
 
 section .bss
 stack_bottom:
