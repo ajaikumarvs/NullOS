34c6ef199259ebfe70ddc191a7c1c2324a407d12
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-16 22:15:01 +0530

    starting stack implementation

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,6 +3,13 @@ global start
 section .text
 bits 32
 start:
- ; print 'OK'
-  mov dword [0xb8000], 0x2f4b2f4f
-  hlt
+    mov esp, stack_top
+
+    ; print 'OK'
+    mov dword [0xb8000], 0x2f4b2f4f
+    hlt
+
+section .bss
+stack_bottom:
+    resb 4069 * 4
+stack_top:
