f5f88fbfb0ddfa4a225636ed20eca3ea9f8d2c8d
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-17 03:17:10 +0530

    changed dir struct

---
+++ target/debug/deps/null_os-70743b904ba0a565.d
@@ -0,0 +1,5 @@
+/home/ak/Desktop/NullOS/target/debug/deps/libnull_os-70743b904ba0a565.rmeta: src/main.rs
+
+/home/ak/Desktop/NullOS/target/debug/deps/null_os-70743b904ba0a565.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/null_os-f398a426e91e1290.d
@@ -0,0 +1,5 @@
+/home/ak/Desktop/NullOS/target/debug/deps/libnull_os-f398a426e91e1290.rmeta: src/main.rs
+
+/home/ak/Desktop/NullOS/target/debug/deps/null_os-f398a426e91e1290.d: src/main.rs
+
+src/main.rs:
